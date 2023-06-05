0ee42ed732e8dcad0d2350e6a4c109b800d46f75
Lalitha pavani <84697520+lalitha4A2@users.noreply.github.com>
2023-06-05 15:57:02 +0530

    import java.util.*; public class avg {     public static void main(String[] args) {         Scanner sc=new Scanner(System.in);         Double a=sc.nextDouble();         Double b=sc.nextDouble();         Double c=(a+b)/2;         System.out.format("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class avg {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Double a=sc.nextDouble();
+        Double b=sc.nextDouble();
+        Double c=(a+b)/2;
+        System.out.format("%.4f",c);
+    }
+}
